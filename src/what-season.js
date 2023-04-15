4386e7fbc764569058bcf41fe273dc0730ab5d88
alima987 <alima1996b@gmail.com>
2023-04-15 17:44:28 +0500

    feat: what seasons

--- src/what-season.js
+++ src/what-season.js
@@ -12,11 +12,48 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function getSeason(date) {
-  const springDate = new Date(2023, 05, 31)
-  const summergDate = new Date(2023, 08, 31)
-  const autumnDate = new Date(2023, 11, 31)
-  const winterDate = new Date(2023, 02, 28)
-  
+ /* date = new Date()
+  let season = date.getMonth()
+  if (typeof season !== 'string') {
+    return false
+  } if (season !== date) {
+    return 'Unable to determine the time of year!';
+  }*/
+  date = new Date()
+   let month = date
+   let season = '';
+    switch(month) {
+        case '12':
+        case '1':
+        case '2':
+            season = 'winter';
+        break;
+        case '3':
+        case '4':
+        case '5':
+            season = 'spring';
+        break;
+        case '6':
+        case '7':
+        case '8':
+            season = 'summer';
+        break;
+        case '9':
+        case '10': 
+        case '11':
+            season = 'autumn';
+        break;
+    }
+    //return season;
+    if (typeof season !== 'string') {
+      return false
+    } if (season !== month) {
+      return 'Unable to determine the time of year!';
+    } if (month !== date) {
+      return 'Invalid date!'
+    } else {
+      return true
+    }
 }
 
 module.exports = {
