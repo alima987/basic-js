2f364f1070090d47802cd75e7be7f39f87a01529
alima987 <alima1996b@gmail.com>
2023-04-15 19:43:56 +0500

    feat: what seasons

--- src/what-season.js
+++ src/what-season.js
@@ -12,48 +12,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function getSeason(date) {
- /* date = new Date()
-  let season = date.getMonth()
-  if (typeof season !== 'string') {
-    return false
-  } if (season !== date) {
-    return 'Unable to determine the time of year!';
-  }*/
-  date = new Date()
-   let month = date
-   let season = '';
-    switch(month) {
-        case '12':
-        case '1':
-        case '2':
-            season = 'winter';
-        break;
-        case '3':
-        case '4':
-        case '5':
-            season = 'spring';
-        break;
-        case '6':
-        case '7':
-        case '8':
-            season = 'summer';
-        break;
-        case '9':
-        case '10': 
-        case '11':
-            season = 'autumn';
-        break;
-    }
-    //return season;
-    if (typeof season !== 'string') {
-      return false
-    } if (season !== month) {
+if (date === undefined) {
       return 'Unable to determine the time of year!';
-    } if (month !== date) {
-      return 'Invalid date!'
-    } else {
-      return true
+    } if (date instanceof Date === false) {
+      throw new Error ('Invalid date!')
+    } try {
+      date.toLocaleString()
+    } catch(err) {
+      if(err) {
+        err = 'Invalid date!'
+      throw new Error ('Invalid date!')
+      }
     }
+    let month = date.getMonth();
+    if (month == 11 || month <= 1) {
+    return 'winter'}
+    else if (month <= 4) {
+       return 'spring'}
+    else if (month <= 7) {
+     return 'summer'}
+    else 
+    {return 'autumn'}
 }
 
 module.exports = {
