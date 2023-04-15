2a043953006e16d0a9e19fb06436d3ecd4f54f3b
alima987 <alima1996b@gmail.com>
2023-04-15 17:10:27 +0500

    feat: carbon dating

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
+function createDreamTeam(members) {
+  const str = members.toString();
+  //members.forEach((str) => {
+    str.charAt(0).toUpperCase() + str.slice(1) + str.join(' ,')
+    if (members = str) {
+      return true
+    } else {
+      false
+    }
+  //})
+  return members
   
-  // remove line with error and write your code here
 }
 
 module.exports = {
